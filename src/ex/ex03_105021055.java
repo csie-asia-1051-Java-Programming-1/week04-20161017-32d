bd0e10e9a83e86daddb2a299709e02883b39e086
32d <105021055@live.asia.edu.tw>
2016-10-17 23:21:50 +0800

    10/17

---
+++ src/ex/ex03_105021055.java
@@ -0,0 +1,26 @@
+﻿package ex;
+/*
+ * Topic: 讓使用者輸入 一個正整數 n 然後顯示出下列圖形 (例 n = 4)
+	* 
+	** 
+	***
+	****
+ * Date: 2016/10/17
+ * Author: 105021055 王浚維
+ */
+import java.util.Scanner;
+public class ex03_105021055 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		for(int x=1;x<=n;x++){
+		for(int z=1;z<=x;z++){
+			System.out.print("*");
+		
+		}
+		System.out.println();
+	}
+}
+}
---
+++ src/ex/ex04_105021055.java
@@ -0,0 +1,28 @@
+﻿package ex;
+/*
+ * Topic: 讓使用者輸入 一個正整數 n 及一個字元符號，然後顯示出下列圖形 (例 n = 4,字元符號= @)
+    @@@@
+    @@@  
+    @@  
+    @
+ * Date: 2016/10/17
+ * Author: 105021055 王浚維
+ */
+import java.util.Scanner;
+public class ex04_105021055 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入一個正數字及一個字元符號");
+		int n = scn.nextInt();
+		char y=scn.next().charAt(0);
+		for(int x=1;x<=n;x++){
+		for(int z=n;z>=x;z--){
+		System.out.print(y);
+		}
+		System.out.println();
+		}
+	}
+
+}
